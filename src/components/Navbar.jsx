3f7d75a6fd4d760b61d38645c984b605b2c5d1ba
Shiv Chauhan <shivchauhan795@gmail.com>
2024-09-03 18:00:53 +0530

    minor change in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Cookies from 'universal-cookie';
 import { useNavigate } from 'react-router-dom';
+import favicon from '../assets/favicon.png'
 
 const Navbar = () => {
     const cookies = new Cookies();
@@ -18,7 +19,7 @@ const Navbar = () => {
         <div className='logo bg-slate-700 h-16 w-full flex flex-wrap items-center text-white px-20 justify-between'>
             <span className='text-xl flex items-center gap-2'>
                 <Link to={'/'}>
-                    Selectify
+                    <h1 className='text-2xl flex justify-center items-center gap-2'><img className='w-10' src={favicon} alt='favicon' />Selectify</h1>
                 </Link>
             </span>
 
